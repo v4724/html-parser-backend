bee0ac286c6f3d59dcea365e93f821a7a1d94fbc
Nydia <nydia.yang@tpisoftware.com>
2025-05-04 19:45:49 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import cors from 'cors';
 
 const app = express();
@@ -18,11 +18,71 @@ app.get('/scrape', async (req, res) => {
 
     const title = $('title').text();
     const description = $('meta[name="description"]').attr('content') || '';
-
+      
+    // 如果只想抓取特定的區塊（例如 class="content" 的區塊）
+    const bookTitle = $('.product-detail-desc-title').text().trim();
+    const previewImg = $('.product-detail-image-main > a > img ').attr('src');
+    let status = '', statusDes = '';
+    const reserve = $('.product-detail-label-item.mk-reserve').html();
+    const stockSufficient = $('.stock_sufficient').html();
+    const stockLittle = $('.stock_little').html();
+    const outOfStock = $('.out_of_stock').html();
+    if (outOfStock) {
+        status = '4';
+        statusDes = 'out_of_stock';
+    } else {
+        if (reserve && stockSufficient) {
+            status = '1';
+            statusDes = 'reserve stock_sufficient';
+        }else if (reserve && stockLittle) {
+            status = '2';
+            statusDes = 'reserve stock_little';
+        }else if (!reserve && stockSufficient) {
+            status = '3';
+            statusDes = '!reserve stock_sufficient';
+        }else if (!reserve && stockLittle) {
+            status = '4';
+            statusDes = '!reserve stock_little';
+        }
+    }
+    let bookTypeAndSize;
+    $('.product-detail-spec-table tr').each((i, el) => {
+    
+        let findSize = false;
+        $(el)
+        .find('td')
+        .each((j, td) => {
+            const text = $(td).text().trim();
+            if (findSize) {
+                bookTypeAndSize = text;
+            }
+            if (text.includes('種別/サイズ')) {
+                findSize = true;
+            }
+        });
+        if (findSize) {
+            return false;
+        }
+        
+    });
+    bookTypeAndSize = bookTypeAndSize.split('/')[1].trim().replaceAll(' ', '');
+    const tmpArr = bookTypeAndSize.split('\n\n');
+    const bookSize = tmpArr[0];
+    const bookPages = tmpArr[1];
+    const price = $('.pricearea__price.pricearea__price--normal').html().split('円')[0].replaceAll(',', '');
+      
     res.json({
       url,
       title,
-      description
+        description,
+        bookTitle,
+        previewImg,
+        status,
+        statusDes,
+        bookSize,
+        bookPages,
+        price
+        
     });
   } catch (error) {
     console.error(error.message);
